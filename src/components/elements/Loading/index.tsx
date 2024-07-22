996f10b9ecc7e7527dcfecc5707e08b09d0d8481
aidahputri <aidahptr15@gmail.com>
2024-07-22 15:53:27 +0700

    feat: init loading componentt

---
+++ src/components/elements/Loading/index.tsx
@@ -0,0 +1,12 @@
+export const Loading: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <div
+        className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full"
+        role="status"
+      >
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  )
+}
