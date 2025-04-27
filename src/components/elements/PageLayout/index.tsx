1fd4fdc19d50ac7e15beaa979e9528e5868fbf06
Aidah Novallia Putri <aidahptr15@gmail.com>
2025-04-27 22:42:38 +0700

    feat: add text color

--- src/components/elements/PageLayout/index.tsx
+++ src/components/elements/PageLayout/index.tsx
@@ -11,7 +11,7 @@ export const PageLayout: React.FC<PageLayoutProps> = ({ children }) => {
   return (
     <AnimatePresence mode="wait">
       <motion.div>
-        <main className="max-w-[1440px] w-full mx-auto flex flex-col justify-center items-center gap-16 md:gap-20 lg:gap-24 px-16 md:px-24 lg:px-36 py-2 md:py-8 xl:py-0">
+        <main className="max-w-[1440px] w-full mx-auto flex flex-col justify-center items-center gap-16 md:gap-20 lg:gap-24 px-16 md:px-24 lg:px-36 py-2 md:py-8 xl:py-0 text-white">
           {children}
         </main>
       </motion.div>
